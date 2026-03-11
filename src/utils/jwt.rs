d2053e8d3db72cf2eddee98c67f35dc4bd1f5623
Asrul Firdaus <asrulfirdaus93@gmail.com>
2026-03-11 12:03:28 +0800

    Added jwt logic to finish the login form

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,10 +1,5 @@
-use jsonwebtoken::{
-    encode, decode, Header, EncodingKey, DecodingKey,
-    Validation, errors::Error as JwtError
-};
 use uuid::Uuid;
 use serde::{Serialize, Deserialize};
-use chrono::{Utc, Duration};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Claims {
@@ -15,7 +10,13 @@ pub struct Claims {
 }
 
 // Helper function to generate jwt token
-pub fn generate_token(user_id: Uuid, user_role: String, permissions: Vec<String>) -> Result<String, JwtError> {
+#[cfg(feature = "ssr")]
+pub fn generate_token(user_id: Uuid, user_role: String, permissions: Vec<String>) -> Result<String, jsonwebtoken::errors::Error> {
+    use jsonwebtoken::{
+        encode, Header, EncodingKey,
+    };
+    use chrono::{Utc, Duration};
+    
     // Set expiration token
     let exp = Utc::now()
         .checked_add_signed(Duration::minutes(10))
@@ -30,7 +31,34 @@ pub fn generate_token(user_id: Uuid, user_role: String, permissions: Vec<String>
             role: user_role,
             permissions,
             exp,
-        }, 
+        },
+        &EncodingKey::from_secret(
+            std::env::var("JWT_SECRET_KEY")
+                .unwrap_or_else(|_| "kost-management-v2".to_string())
+                .as_ref()
+        ) 
         
     )
+}
+
+
+#[cfg(feature = "ssr")]
+pub fn verify_token(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
+    use jsonwebtoken::{
+        decode, Header, DecodingKey,
+        Validation,
+    };
+    
+    // Decode the token and verify
+    let token_data = decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(
+            std::env::var("JWT_SECRET_KEY")
+                .unwrap_or_else(|_| "kost-management-v2".to_string())
+                .as_ref()   
+        ),
+        &Validation::default(),
+    )?;
+
+    Ok(token_data.claims)
 }
